33d0101cdabce736c62f890ecd9747005b7af80f
Tim Perry <pimterry@gmail.com>
2016-07-01 17:34:00 +0200

    Locally-working github notification reading

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 'use strict';
 
 var getJoke = require("jokes");
+var GithubApi = require("github");
+var github = new GithubApi();
 
 exports.handler = function (event, context) {
-    console.log(event);
-    console.log(getJoke().text);
-    context.succeed("Joked!");
+    console.info("Triggering event", event);
+
+    github.authenticate({
+        type: "oauth",
+        token: process.env.GITHUB_ACCESS_TOKEN
+    });
+
+    github.activity.getNotifications({
+    }).then((notifications) => {
+        if (notifications.length > 0) {
+            console.log(getJoke().text);
+        }
+
+        context.succeed(notifications);
+    }).catch(context.fail);
 }
