0fdcf8a4c1631a3379f079340a2c51688b572074
Tim Perry <pimterry@gmail.com>
2016-07-01 19:54:09 +0200

    Working bot! Surprisingly funny too.

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var GithubApi = require("github");
 var github = new GithubApi();
 
 exports.handler = function (event, context) {
-    console.info("Triggering event", event);
-
     github.authenticate({
         type: "oauth",
         token: event.github_token
@@ -15,8 +13,38 @@ exports.handler = function (event, context) {
     github.activity.getNotifications({
     }).then((notifications) => {
         if (notifications.length > 0) {
-            console.log(getJoke().text);
-            return github.activity.markNotificationThreadAsRead({ id: notifications[0].id });
+            return Promise.all(notifications.filter((notification) => {
+                return notification.unread === true &&
+                       notification.reason === 'mention' &&
+                       notification.subject.type === 'Issue';
+            }).map((notification) => {
+                var issueUrl = notification.subject.url;
+                var issueMatch = /api.github.com\/repos\/(\w+)\/(\w+)\/issues\/(\d+)/.exec(issueUrl);
+                if (issueMatch) {
+                    var username = issueMatch[1];
+                    var repo = issueMatch[2];
+                    var issueId = issueMatch[3];
+
+                    return github.issues.createComment({
+                        user: username,
+                        repo: repo,
+                        number: issueId,
+                        body: getJoke().text
+                    });
+                } else {
+                    throw new Error(`Failed to match issue URL: ${issueUrl}`);
+                }
+            })).then(() => {
+                // Is there a race condition here? (if thread gets a new message between above and here)
+                return Promise.all(notifications.map((notification) => {
+                    return github.activity.setNotificationThreadSubscription({
+                        id: notification.id,
+                        ignored: true
+                    });
+                }).concat(notifications.map((notification) => {
+                    return github.activity.markNotificationThreadAsRead({ id: notification.id });
+                })));
+            });
         }
     }).then(() => context.succeed()).catch(context.fail);
 }
